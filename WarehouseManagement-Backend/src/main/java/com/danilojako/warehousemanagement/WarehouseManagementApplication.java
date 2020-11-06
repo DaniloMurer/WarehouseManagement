b6a1fa26ddfe97c6fe350337971e40391a783f93
Danilo Jakob <danilo.murer@gmail.com>
2020-11-06 15:56:50 +0100

    CHANGE: Project-Setup of frontend and backend

---
+++ WarehouseManagement-Backend/src/main/java/com/danilojako/warehousemanagement/WarehouseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.danilojako.warehousemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagementApplication.class, args);
+	}
+
+}
---
+++ WarehouseManagement-Backend/src/test/java/com/danilojako/warehousemanagement/WarehouseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilojako.warehousemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
